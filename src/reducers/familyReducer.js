54ba9e1d2af352b5c40ab08a5a2084cfe9b3620c
nubol23 <rafael.rvp98@gmail.com>
2022-04-05 14:36:15 -0400

    added family context ad reducer

---
+++ src/reducers/familyReducer.js
@@ -0,0 +1,30 @@
+import familyTypes from "../types/familyTypes";
+
+// family := {
+//   id,
+//   name,
+// }
+
+export const familyReducer = (state = {}, action) => {
+  switch (action.type) {
+    case familyTypes.addFamily:
+      return [...state, action.payload];
+
+    case familyTypes.removeFamily:
+      return state.filter(family => family.id !== action.payload.id);
+
+    case familyTypes.updateFamily:
+      return state.map(family => {
+        if (family.id === action.payload.id) {
+          return {
+            ...family,
+            name: action.payload.name
+          }
+        }
+        return family
+      })
+
+    default:
+      return state
+  }
+}
---
+++ src/types/familyTypes.js
@@ -0,0 +1,7 @@
+const familyTypes = {
+  addFamily: '[family] add',
+  removeFamily: '[family] remove',
+  updateFamily: '[family] update',
+}
+
+export default familyTypes;
