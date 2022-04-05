01281afcc55fef4802f4e0a132da506add2558e2
nubol23 <rafael.rvp98@gmail.com>
2022-04-04 20:58:34 -0400

    Refactored app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import {useEffect, useReducer} from "react";
+import {BrowserRouter} from "react-router-dom";
+import {AuthContext} from "./auth/authContext";
+import {authReducer} from "./auth/authReducer";
+import {AppRouter} from "./routers/AppRouter";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('user') || '{"logged": false}')
+}
+
 function App() {
+  const [user, userDispatch] = useReducer(authReducer, {}, init);
+
+  useEffect(() => {
+    if (!user) return;
+
+    localStorage.setItem('user', JSON.stringify(user));
+  }, [user])
+
   return (
-    <div>Hello World</div>
+    <AuthContext.Provider value={{user, userDispatch}}>
+      <BrowserRouter>
+        <AppRouter/>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
