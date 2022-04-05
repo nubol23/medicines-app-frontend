4350ea2da7743a9cd22375c79eaeac4b795b5cbe
nubol23 <rafael.rvp98@gmail.com>
2022-04-04 20:30:51 -0400

    added auth reducer and context

---
+++ src/auth/authReducer.js
@@ -0,0 +1,20 @@
+import authTypes from "../types/authTypes";
+
+
+export const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case authTypes.login:
+      return {
+        ...action.payload,
+        logged: true,
+      }
+
+    case authTypes.logout:
+      return {
+        logged: false,
+      }
+
+    default:
+      return state;
+  }
+}
