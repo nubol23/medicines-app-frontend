67844e1eafb24e57c0c1ffdf21a7e8e18e87cbb7
nubol23 <rafael.rvp98@gmail.com>
2022-04-05 22:52:56 -0400

    added create family

--- src/components/FamilyScreen/index.js
+++ src/components/FamilyScreen/index.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import "./family.scss"
 import FamilyTable from "../FamilyTable";
 import useForm from "../../hooks/useForm";
+import api from "../../apis/api";
+import {AuthContext} from "../../auth/authContext";
+import {FamilyContext} from "../../contexts/familyContext";
+import familyTypes from "../../types/familyTypes";
+import authTypes from "../../types/authTypes";
 
 const FamilyScreen = () => {
+
+  const {userDispatch} = useContext(AuthContext);
+  const {familiesDispatch} = useContext(FamilyContext);
 
   const [{new_family_name}, handleInputChange] = useForm({new_family_name: '',})
 
   const handleCreateFamily = (e) => {
     e.preventDefault();
 
-    console.log("CREATE")
+    api.post("/families/", {
+      family_name: new_family_name,
+    })
+      .then((response) => {
+        familiesDispatch({
+          type: familyTypes.addFamily,
+          payload: response.data,
+        })
+      })
+      .catch((error) => {
+
+        console.log("Error creating")
+
+        // If returned 401
+        if (error.response && error.response.status === 401)
+          userDispatch({type: authTypes.logout});
+      })
   }
 
   return (
