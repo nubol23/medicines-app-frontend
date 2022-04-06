96f91fb49d47923626ebe65bac97a4b2368dddc2
nubol23 <rafael.rvp98@gmail.com>
2022-04-05 22:15:07 -0400

    quick fix login timeot

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -8,18 +8,22 @@ const useRequest = (httpRequest, resolveFunc, rejectFunc, deps = []) => {
   const {userDispatch} = useContext(AuthContext);
 
   useEffect(() => {
-    httpRequest
-      .then((response) => {
-        resolveFunc(response);
-      })
-      .catch((error) => {
-        rejectFunc(error);
+    setTimeout(() => {
 
-        // If returned 401
-        if (error.response && error.response.status === 401)
-          userDispatch({type: authTypes.logout});
+      httpRequest
+        .then((response) => {
+          resolveFunc(response);
+        })
+        .catch((error) => {
+          rejectFunc(error);
 
-      })
+          // If returned 401
+          if (error.response && error.response.status === 401)
+            userDispatch({type: authTypes.logout});
+
+        })
+
+    }, 100)
   }, deps)
 };
 
