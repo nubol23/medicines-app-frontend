4a6102f7543e8021c421a3fc4a80c056ff7b1848
nubol23 <rafael.rvp98@gmail.com>
2022-04-06 22:12:31 -0400

    added medicine form

--- src/components/MedicinesScreen/index.js
+++ src/components/MedicinesScreen/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import useForm from "../../hooks/useForm";
+import "./medicines.scss"
 
 const MedicinesScreen = () => {
+
+  const [{medicineName}, handleInputChange, reset] = useForm({medicineName: '',})
+
+  const handleSearchMedicine = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
     <div>
-      Medicines Screen
+      <div className="medicines-buttons">
+        <form className="search-medicine-form" onSubmit={handleSearchMedicine}>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Medicamento"
+            name="medicineName"
+            value={medicineName}
+            onChange={handleInputChange}
+          />
+
+          <button type="submit" className="create-family-button">Buscar</button>
+        </form>
+
+        <div>
+          <button className="create-family-button">Crear</button>
+        </div>
+      </div>
     </div>
   );
 };
