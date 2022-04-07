8ac5815d086904117d1c483dea3b846925ebe03c
nubol23 <rafael.rvp98@gmail.com>
2022-04-07 14:39:44 -0400

    added medicine context and reducer

---
+++ src/types/medicineTypes.js
@@ -0,0 +1,9 @@
+const medicineTypes = {
+  add: '[medicine] add',
+  remove: '[medicine] remove',
+  update: '[medicine] update',
+  addMultiple: '[medicine] multiple',
+  clear: '[medicine] clear',
+}
+
+export default medicineTypes;
