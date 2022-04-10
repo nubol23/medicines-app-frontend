dc5da2010d15827ce7c87d7b89ac1082df1ee2d5
nubol23 <rafael.rvp98@gmail.com>
2022-04-10 00:24:52 -0400

    added purchase data in table

--- src/components/PurchaseTable/index.js
+++ src/components/PurchaseTable/index.js
@@ -1,11 +1,35 @@
 import React, {useContext, useState} from 'react';
 import {PurchaseContext} from "../../contexts/purchaseContext";
 import LoadingCircle from "../LoadingCircle";
+import useRequest from "../../hooks/useRequest";
+import api from "../../apis/api";
+import purchaseTypes from "../../types/purchaseTypes";
+import {formatDate} from "../../utils/functions";
 
 const PurchaseTable = ({familyId}) => {
 
   const {purchases, purchasesDispatch} = useContext(PurchaseContext);
   const [loading, setLoading] = useState(true)
+
+  useRequest(
+    () => api.get("/medicines/purchase"),
+    (response) => {
+      purchasesDispatch({type: purchaseTypes.clear});
+      purchasesDispatch({
+        type: purchaseTypes.addMultiple,
+        payload: response.data.results,
+      });
+      setLoading(false)
+      // setPaginatorParams({
+      //   ...paginatorParams,
+      //   totalCount: response.data.count,
+      //   nextUrl: response.data.next,
+      //   prevUrl: response.data.previous,
+      // })
+    },
+    (error) => {
+    },
+  )
 
   const handleTableClick = (purchaseId) => {
 
@@ -26,9 +50,9 @@ const PurchaseTable = ({familyId}) => {
         <table className="table table-hover">
           <thead>
           <tr>
-            <th>Nombre</th>
-            <th>Fabricante</th>
-            <th>Cant.</th>
+            <th>Medicina</th>
+            <th>Familia</th>
+            <th>Expiración</th>
             <th>U.</th>
             <th></th>
           </tr>
@@ -37,7 +61,12 @@ const PurchaseTable = ({familyId}) => {
           {
             purchases.map(purchase => (
               <tr key={purchase.id} className="animate__animated animate__fadeIn">
-                <td onClick={() => handleTableClick(purchase.id)}>AAA</td>
+                <td onClick={() => handleTableClick(purchase.id)}>
+                  {purchase.medicine.name} - {purchase.medicine.maker}: {purchase.medicine.quantity} {purchase.medicine.unit}
+                </td>
+                <td onClick={() => handleTableClick(purchase.id)}>{purchase.family.family_name}</td>
+                <td onClick={() => handleTableClick(purchase.id)}>{formatDate(purchase.buy_date)}</td>
+                <td onClick={() => handleTableClick(purchase.id)}>{purchase.units}</td>
                 <td>
                   <button
                     className="edit-row-button"
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -3,3 +3,10 @@ export const emptyObj = (obj) => {
     && Object.keys(obj).length === 0
     && Object.getPrototypeOf(obj) === Object.prototype
 }
+
+export const formatDate = (dateStr) => {
+  let res = dateStr.split("T").at(0)
+  const parts = res.split("-")
+  res = `${parts.at(2)}/${parts.at(1)}/${parts.at(0)}`
+  return res
+}
