1a82ab6a89daec701211342cadf06a614135bea0
nubol23 <rafael.rvp98@gmail.com>
2022-04-09 22:59:43 -0400

    added search purchase by medicine skeleton

--- src/components/PurchasesScreen/index.js
+++ src/components/PurchasesScreen/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import useForm from "../../hooks/useForm";
+import "./purchases.scss"
 
 const PurchasesScreen = () => {
+
+  const [{medicineName}, handleInputChange] = useForm({medicineName: '',})
+
+  const handleSearchByMedicine = (e) => {
+    e.preventDefault();
+    
+  }
+
   return (
-    <div>
-      Purchases Screen
+    <div className="medicine-screen animate__animated animate__fadeIn">
+
+      <div className="medicines-buttons">
+        <form className="search-purchases-form" onSubmit={handleSearchByMedicine}>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Buscar por medicamento"
+            name="medicineName"
+            value={medicineName}
+            onChange={handleInputChange}
+          />
+
+          <button type="submit" className="create-family-button">Buscar</button>
+        </form>
+      </div>
+
     </div>
   );
 };
