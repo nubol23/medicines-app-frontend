86d5f357a7cf0b7e9ee74eb1abddd1235068c8cb
nubol23 <rafael.rvp98@gmail.com>
2022-04-09 23:44:02 -0400

    added purchase context and reducer

---
+++ src/types/purchaseTypes.js
@@ -0,0 +1,9 @@
+const purchaseTypes = {
+  add: '[purchase] add',
+  remove: '[purchase] remove',
+  update: '[purchase] update',
+  addMultiple: '[purchase] multiple',
+  clear: '[purchase] clear',
+}
+
+export default purchaseTypes;
