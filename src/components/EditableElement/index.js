0e99e52d18799026cd181518593f713be59e9c0a
nubol23 <rafael.rvp98@gmail.com>
2022-04-11 19:45:33 -0400

    created editable element component

---
+++ src/components/EditableElement/index.js
@@ -0,0 +1,33 @@
+import React, {useRef} from "react";
+import useUpdateEffect from "../../hooks/useUpdateEffect";
+
+const EditableElement = (props) => {
+  const {onChange} = props;
+  const element = useRef();
+
+  let elements = React.Children.toArray(props.children);
+  if (elements.length > 1) {
+    throw Error("Can't have more than one child");
+  }
+
+  const onMouseUp = () => {
+    const value = element.current?.value || element.current?.innerText;
+    onChange(value);
+  };
+
+  useUpdateEffect(() => {
+    const value = element.current?.value || element.current?.innerText;
+    onChange(value);
+  }, []);
+
+  elements = React.cloneElement(elements[0], {
+    contentEditable: true,
+    suppressContentEditableWarning: true,
+    ref: element,
+    onKeyUp: onMouseUp
+  });
+
+  return elements;
+};
+
+export default EditableElement;
