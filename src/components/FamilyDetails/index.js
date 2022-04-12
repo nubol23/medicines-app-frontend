4b14236b3609b4c754479362cb953a0899cfda29
nubol23 <rafael.rvp98@gmail.com>
2022-04-11 20:45:17 -0400

    created family detail skeleton

--- src/components/FamilyDetails/index.js
+++ src/components/FamilyDetails/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useParams} from "react-router-dom";
+import useRequest from "../../hooks/useRequest";
+import api from "../../apis/api";
+import "./familyDetails.scss"
 
 const FamilyDetails = () => {
 
   const {familyId} = useParams();
+  const [family, setFamily] = useState({id: familyId, family_name: ""});
+
+  useRequest(
+    () => api.get(`/families/${familyId}`),
+    (response) => {
+      setFamily(response.data)
+    },
+    (error) => {
+    },
+  )
+
 
   return (
-    <div>
-      Family {familyId} details
+    <div className="medicine-screen">
+      <div className="family-editable-row">
+        <h4 className="editable-name">
+          {family.family_name}
+        </h4>
+
+        <button className="primary-button">Invitar</button>
+      </div>
     </div>
   );
 };
