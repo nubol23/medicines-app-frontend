ce003a60f34a4b0a5912b61834e5d23b81446308
nubol23 <rafael.rvp98@gmail.com>
2022-04-15 16:55:47 -0400

    added create user logic:

--- src/components/RegisterScreen/index.js
+++ src/components/RegisterScreen/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, {useState} from 'react';
+import {Link, useNavigate} from "react-router-dom";
 import useForm from "../../hooks/useForm";
+import {toast} from "react-hot-toast";
+import authApi from "../../apis/authApi";
 
 const RegisterScreen = () => {
 
@@ -12,9 +14,39 @@ const RegisterScreen = () => {
     password: "",
     confirmPassword: "",
   })
+  const [disabled, setDisabled] = useState(false);
 
+  const navigate = useNavigate();
   const handleRegister = (e) => {
     e.preventDefault();
+
+    if (email === "" || firstName === "" || lastName === "" || phoneNumber === "" || password === "" || confirmPassword === "") {
+      toast.error("Todos los campos son obligatorios")
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      toast.error("Las contraseñas no son iguales")
+      return;
+    }
+
+    setDisabled(true);
+    authApi.post("/users/register", {
+      email,
+      first_name: firstName,
+      last_name: lastName,
+      phone_number: phoneNumber,
+      password,
+    })
+      .then((response) => {
+        setDisabled(false);
+        toast.success("Éxito! revise su correo")
+        navigate("/login", {replace: true})
+      })
+      .catch((error) => {
+        toast.error("Error al crear la cuenta")
+        setDisabled(false);
+      })
   }
 
   return (
@@ -22,7 +54,7 @@ const RegisterScreen = () => {
 
       <div className="login-content">
 
-        <h3 className="login-header mb-4">Medicines App</h3>
+        <h3 className="login-header mb-4">Crear cuenta</h3>
 
         <form onSubmit={handleRegister} className="form-box">
           <input
@@ -81,7 +113,7 @@ const RegisterScreen = () => {
             onChange={handleInputChange}
           />
 
-          <button type='submit' className="primary-button">
+          <button type='submit' className="primary-button" disabled={disabled}>
             Registrarse
           </button>
           <Link to="/login">¿Ya tienes cuenta?</Link>
