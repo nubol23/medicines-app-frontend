99ea8d0b0fa91d8c3da5cc42e411c3c14f28a3a5
nubol23 <rafael.rvp98@gmail.com>
2022-04-20 17:01:02 -0400

    added enviorement vars file

--- src/apis/api.js
+++ src/apis/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const api = setUpInterceptorsTo(
   axios.create({
-    baseURL: "http://127.0.0.1:8000/",
+    baseURL: process.env.REACT_APP_BASE_URL,
     headers: {
       "Content-Type": "application/json",
     },
--- src/apis/authApi.js
+++ src/apis/authApi.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 const authApi = axios.create({
-  baseURL: "http://127.0.0.1:8000/"
+  baseURL: process.env.REACT_APP_BASE_URL
 });
 export default authApi;
