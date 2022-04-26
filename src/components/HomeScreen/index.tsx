69d4f7718e69f02e7d975f572a8bd5eddace7080
nubol23 <rafael.rvp98@gmail.com>
2022-04-26 19:49:47 -0400

    migrated home screen

---
+++ src/components/HomeScreen/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.scss";
+
+export const HomeScreen = () => {
+  return (
+    <div>
+      <h1 className="home-header">Bienvenido a medicines app</h1>
+
+      <p>Gestiona los medicamentos de tu familia</p>
+    </div>
+  );
+};
