1468279acafd57ce81dee3e4d101d49c5098b64b
nubol23 <rafael.rvp98@gmail.com>
2022-05-01 20:37:07 -0400

    migrated register form to formik

--- src/screens/RegisterScreen/index.tsx
+++ src/screens/RegisterScreen/index.tsx
@@ -1,61 +1,58 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import useForm from "../../hooks/useForm";
 import { toast } from "react-hot-toast";
 import authApi from "../../apis/authApi";
 import { AxiosError } from "axios";
+import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik";
+import * as Yup from "yup";
+
+type RegisterFormValues = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const RegisterSchema = Yup.object().shape({
+  email: Yup.string().email("Formato de correo inválido").required("Requerido"),
+  firstName: Yup.string().required("Requerido"),
+  lastName: Yup.string().required("Requerido"),
+  phoneNumber: Yup.string().required("Requerido"),
+  password: Yup.string().required("Requerido"),
+  confirmPassword: Yup.string().required("Requerido"),
+});
+
+const validateRegisterForm = (values: RegisterFormValues) => {
+  let errors = {};
+  if (values.password !== values.confirmPassword) {
+    errors = {
+      ...errors,
+      password: "Las contraseñas no coinciden",
+      confirmPassword: "Las contraseñas no coinciden",
+    };
+  }
+
+  return errors;
+};
 
 const RegisterScreen = () => {
-  const [
-    { email, firstName, lastName, phoneNumber, password, confirmPassword },
-    handleInputChange,
-  ] = useForm({
-    email: "",
-    firstName: "",
-    lastName: "",
-    phoneNumber: "",
-    password: "",
-    confirmPassword: "",
-  });
-  const [disabled, setDisabled] = useState(false);
-
   const navigate = useNavigate();
-  const handleRegister = (e: React.FormEvent) => {
-    e.preventDefault();
 
-    if (
-      email === "" ||
-      firstName === "" ||
-      lastName === "" ||
-      phoneNumber === "" ||
-      password === "" ||
-      confirmPassword === ""
-    ) {
-      toast.error("Todos los campos son obligatorios");
-      return;
-    }
-
-    if (password !== confirmPassword) {
-      toast.error("Las contraseñas no son iguales");
-      return;
-    }
-
-    if (!/^[A-Z\d._%+-]+@[A-Z\d.-]+\.[A-Z]{2,}$/i.test(email)) {
-      toast.error("Email inválido");
-      return;
-    }
-
-    setDisabled(true);
+  const handleRegister = (
+    values: RegisterFormValues,
+    { setSubmitting }: FormikHelpers<RegisterFormValues>
+  ) => {
     authApi
       .post("/users/register", {
-        email,
-        first_name: firstName,
-        last_name: lastName,
-        phone_number: phoneNumber,
-        password,
+        email: values.email,
+        first_name: values.firstName,
+        last_name: values.lastName,
+        phone_number: values.phoneNumber,
+        password: values.password,
       })
       .then((response) => {
-        setDisabled(false);
         toast.success("Éxito! revise su correo para activar su cuenta");
         navigate("/login", { replace: true });
       })
@@ -63,15 +60,12 @@ const RegisterScreen = () => {
         if (
           error.response?.data?.email?.at(0) ===
           "user with this email address already exists."
-        ) {
+        )
           toast.error("Ya existe un usuario con este correo.");
-          setDisabled(false);
-          return;
-        }
-
-        toast.error("Error al crear la cuenta");
-        setDisabled(false);
+        else toast.error("Error al crear la cuenta");
       });
+
+    setSubmitting(false);
   };
 
   return (
@@ -79,68 +73,116 @@ const RegisterScreen = () => {
       <div className="login-content">
         <h3 className="login-header mb-4">Crear cuenta</h3>
 
-        <form onSubmit={handleRegister} className="form-box">
-          <input
-            className="form-control mb-4"
-            type="text"
-            placeholder="Correo"
-            name="email"
-            value={email}
-            onChange={handleInputChange}
-          />
+        <Formik
+          initialValues={{
+            email: "",
+            firstName: "",
+            lastName: "",
+            phoneNumber: "",
+            password: "",
+            confirmPassword: "",
+          }}
+          validationSchema={RegisterSchema}
+          validate={validateRegisterForm}
+          onSubmit={handleRegister}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <div className="mb-4">
+                <Field
+                  type="email"
+                  name="email"
+                  placeholder="Correo"
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="error-message"
+                />
+              </div>
 
-          <input
-            className="form-control mb-4"
-            type="text"
-            placeholder="Nombres"
-            name="firstName"
-            value={firstName}
-            onChange={handleInputChange}
-          />
+              <div className="mb-4">
+                <Field
+                  type="text"
+                  name="firstName"
+                  placeholder="Nombres"
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="firstName"
+                  component="div"
+                  className="error-message"
+                />
+              </div>
 
-          <input
-            className="form-control mb-4"
-            type="text"
-            placeholder="Apellidos"
-            name="lastName"
-            value={lastName}
-            onChange={handleInputChange}
-          />
+              <div className="mb-4">
+                <Field
+                  type="text"
+                  name="lastName"
+                  placeholder="Apellidos"
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="lastName"
+                  component="div"
+                  className="error-message"
+                />
+              </div>
 
-          <input
-            className="form-control mb-4"
-            type="text"
-            placeholder="Teléfono"
-            name="phoneNumber"
-            value={phoneNumber}
-            onChange={handleInputChange}
-          />
+              <div className="mb-4">
+                <Field
+                  type="text"
+                  name="phoneNumber"
+                  placeholder="Número de teléfono"
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="phoneNumber"
+                  component="div"
+                  className="error-message"
+                />
+              </div>
 
-          <input
-            className="form-control mb-4"
-            type="password"
-            placeholder="Contraseña"
-            name="password"
-            autoComplete="off"
-            value={password}
-            onChange={handleInputChange}
-          />
+              <div className="mb-4">
+                <Field
+                  type="password"
+                  name="password"
+                  placeholder="Constraseña"
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="error-message"
+                />
+              </div>
 
-          <input
-            className="form-control mb-4"
-            type="password"
-            placeholder="Confirmar contraseña"
-            name="confirmPassword"
-            autoComplete="off"
-            value={confirmPassword}
-            onChange={handleInputChange}
-          />
+              <div className="mb-4">
+                <Field
+                  type="password"
+                  name="confirmPassword"
+                  placeholder="Confirmar contraseña"
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="confirmPassword"
+                  component="div"
+                  className="error-message"
+                />
+              </div>
 
-          <button type="submit" className="primary-button" disabled={disabled}>
-            Registrarse
-          </button>
-          <Link to="/login">¿Ya tienes cuenta?</Link>
-        </form>
+              <button
+                type="submit"
+                className="primary-button"
+                disabled={isSubmitting}
+              >
+                Registrar
+              </button>
+            </Form>
+          )}
+        </Formik>
+        <Link to="/login">¿Ya tienes cuenta?</Link>
       </div>
     </div>
   );
